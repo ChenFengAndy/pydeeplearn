ca347bc5fd717b0e080ee8f3ca363de078e9020f
Mihaela Rosca <mihaelacrosca@gmail.com>
2014-02-09 14:23:04 +0000

    Back to usual thing with nestervo momentum implemented

--- code/deepbelief.py
+++ code/deepbelief.py
@@ -142,8 +142,8 @@ class DBN(object):
         # Update the weights and biases using gradient descent
         # Also update the old weights
         for index in xrange(stages):
-          oldDWeights[index] = momentum * oldDWeights[index] - batchLearningRate * dWeights[index]
-          oldDBias[index] = momentum * oldDBias[index] - batchLearningRate * dBias[index]
+          oldDWeights[index] = momentum * oldDWeights[index] + batchLearningRate * dWeights[index]
+          oldDBias[index] = momentum * oldDBias[index] + batchLearningRate * dBias[index]
           self.weights[index] -=  batchLearningRate * dWeights[index]
           self.biases[index] -= batchLearningRate * dBias[index]
 
