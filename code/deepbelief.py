7a1d17f615ffe6d5b58e96c8e3fb1ac62f4d14b5
Mihaela Rosca <mihaelacrosca@gmail.com>
2014-02-09 14:59:28 +0000

    Changed momentum to the paper way

--- code/deepbelief.py
+++ code/deepbelief.py
@@ -135,10 +135,10 @@ class DBN(object):
         # Update the weights and biases using gradient descent
         # Also update the old weights
         for index in xrange(stages):
-          oldDWeights[index] = momentum * oldDWeights[index] + batchLearningRate * dWeights[index]
-          oldDBias[index] = momentum * oldDBias[index] + batchLearningRate * dBias[index]
-          self.weights[index] -= oldDWeights[index]
-          self.biases[index] -= oldDBias[index]
+          oldDWeights[index] = momentum * oldDWeights[index] - batchLearningRate * dWeights[index]
+          oldDBias[index] = momentum * oldDBias[index] - batchLearningRate * dBias[index]
+          self.weights[index] += oldDWeights[index]
+          self.biases[index] += oldDBias[index]
 
 
   def classify(self, dataInstaces):
