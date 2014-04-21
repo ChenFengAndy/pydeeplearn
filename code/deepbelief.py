3408d1f0876bb366f07a0a8a2b91fbde74d3cea3
Mihaela Rosca <mihaelacrosca@gmail.com>
2014-04-21 20:31:40 +0100

    0.9 dicrease

--- code/deepbelief.py
+++ code/deepbelief.py
@@ -416,7 +416,7 @@ class DBN(object):
 
       momentum = np.float32(min(np.float32(0.5) + epoch * np.float32(0.01),
                      np.float32(0.99)))
-      learningRate*= 0.99
+      learningRate*= 0.9
       learningRate = np.float32(learningRate)
 
       for batchNr in xrange(self.nrMiniBatches):
