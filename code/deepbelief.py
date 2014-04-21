e92f4a9ccd67e2839e58435637417a7936187cca
Mihaela Rosca <mihaelacrosca@gmail.com>
2014-04-21 18:11:20 +0100

    start with 0.01

--- code/deepbelief.py
+++ code/deepbelief.py
@@ -410,7 +410,7 @@ class DBN(object):
 
 
   def trainLoopModelFixedEpochs(self, batchTrainer, trainModel, maxEpochs):
-    learningRate = np.float32(0.1)
+    learningRate = np.float32(0.01)
     for epoch in xrange(maxEpochs):
       print "epoch " + str(epoch)
 
